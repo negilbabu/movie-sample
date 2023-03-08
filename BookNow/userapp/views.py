ff723d01116291321acce592d3f43262eb53bbd6
Manu <manu.ravi@innovaturelabs.com>
2023-03-08 18:40:45 +0530

    sample view creation

--- BookNow/userapp/views.py
+++ BookNow/userapp/views.py
@@ -1,3 +1,10 @@
+import datetime
+from django.http import HttpResponse
+from django.urls import include,path
 from django.shortcuts import render
-
+from userapp import views
 # Create your views here.
+def current_datetime(request):
+    now=datetime.datetime.now()
+    html="<html><body>It is now %s. </body></html>" % now
+    return HttpResponse(html)
